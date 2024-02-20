00d3d4132721b0f6bd789e263260d009e9f5364e
Ayush Kumar <ayushkumar982191@gmail.com>
2024-02-20 21:43:15 +0530

    Test commit

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import LogoImage from "../../assets/logo.png"
 
 function Logo() {
-  return <img src={LogoImage} width={65} alt="QTify Logo" />
+  return <img src={LogoImage}  width={65} style={{marginLeft:"2rem"}} alt="QTify Logo" />
 }
 
 export default Logo
