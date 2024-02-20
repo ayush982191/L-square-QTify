14fb1a5737092e75a4a12bf7a1d7f025190262ed
Ayush Kumar <ayushkumar982191@gmail.com>
2024-02-20 21:09:34 +0530

    Test commit

--- qtify/src/components/SearchBar/SearchBar.jsx
+++ qtify/src/components/SearchBar/SearchBar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-// import { ReactComponent as SearchIcon} from "../../assets/search-icon.svg"
+import SearchIcon from "../../assets/search-icon.svg"
+import style  from "./SearchBar.module.css"
 
 function SearchBar() {
   return (
-    <div>
+    <div style={{display:"flex"}} >
         <input type="text"  placeholder='Search a song of your choice' style={{height:"2rem",borderRadius:"6px",outline:"none",width:"25rem" }} />      
-      {/* <SearchIcon/>                                                 */}
+      <button className={style.button} style={{borderRadius:"6px",backgroundColor:"white",width:"40px",height:"2.3rem"}} ><img src={SearchIcon} alt="" /></button>                                               
     </div>
   )
 }
