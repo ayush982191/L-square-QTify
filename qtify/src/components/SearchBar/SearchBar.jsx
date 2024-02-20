31f5308529b501df2a7c7057d9cf867c0a46fe92
Ayush Kumar <ayushkumar982191@gmail.com>
2024-02-20 21:22:12 +0530

    Test commit

--- qtify/src/components/SearchBar/SearchBar.jsx
+++ qtify/src/components/SearchBar/SearchBar.jsx
@@ -4,7 +4,7 @@ import style  from "./SearchBar.module.css"
 
 function SearchBar() {
   return (
-    <div style={{display:"flex"}} >
+    <div style={{display:"flex", alignItems:"center"}} >
         <input type="text"  placeholder='Search a song of your choice' style={{height:"2rem",borderRadius:"6px",outline:"none",width:"25rem" }} />      
       <button className={style.button} style={{borderRadius:"6px",backgroundColor:"white",width:"40px",height:"2.3rem"}} ><img src={SearchIcon} alt="" /></button>                                               
     </div>
