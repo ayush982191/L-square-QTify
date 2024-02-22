dc2a4edc97c7d92bbc018257b248cc41289cdb42
Ayush Kumar <ayushkumar982191@gmail.com>
2024-02-22 23:10:14 +0530

    two cards

--- qtify/src/components/Section/Section.jsx
+++ qtify/src/components/Section/Section.jsx
@@ -9,16 +9,13 @@ import Container from '@mui/material/Container';
 
 function Section() {
     const [apiData, setApiData] = useState([]);
+    const [newAlbum,setNewAlbum]=useState([]);
 
     const fetchData = async () => {
         console.log("Enter in fetchData");
         try {
              const response = await axios.get('https://qtify-backend-labs.crio.do/albums/top');
-            // setApiData(response.data);
-            // console.log("api data is",apiData[0]);
-            // console.log("api data is ",apiData);
-            // console.log("response coming is ",response);
-            // console.log("response coming is ",response);
+              
             console.log("response data is ",response.data);
             console.log("response data length is ",response.data.length);
             setApiData(response.data);
@@ -27,9 +24,22 @@ function Section() {
              console.error('Error fetching data:', error);
         }                                                                                                                           
     };
+    const fetchNewData=async ()=>{
+        try {
+            const response = await axios.get('https://qtify-backend-labs.crio.do/albums/new');
+             
+           console.log("response data is ",response.data);
+           console.log("response data length is ",response.data.length);
+           setNewAlbum(response.data);
+
+       } catch (error) {
+            console.error('Error fetching data:', error);
+       }    
+    }
 
     useEffect(() => {
         fetchData();
+        fetchNewData();
     }, []);
 
     return (
@@ -48,6 +58,18 @@ function Section() {
                 </div>
                 </div>
                 {/* </Container> */}
+                <div className={style.mainDiv}   >
+             
+                <div className={style.albumText}  >
+                    <h1 >New Album</h1>
+                    <button >Collapse</button>
+                </div>
+                <div className={style.cardSection} >
+                    {newAlbum.map(item => ( 
+                        <Card  item={item} />
+                    ))}
+                </div>
+                </div>
         </>
     );
 }
